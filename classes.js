6cc61fe53298133806d1341445ba8d60f29a90fb
elliot <elliotgcraig@icloud.com>
2019-01-09 14:31:26 -0700

    finished three problems

--- classes.js
+++ classes.js
@@ -31,7 +31,17 @@
 
 //Code Here
 
-
+class Employee {
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name
+    this.last_name = last_name
+    this.email = email
+    this.age = age
+  }
+  makeWidget(){
+    return `${this.first_name} ${this.last_name} Widget`
+  }
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -51,7 +61,24 @@
 
 //Code Here
 
-
+class Manager {
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name
+    this.last_name = last_name
+    this.email = email
+    this.age = age
+    this.reports = []
+  }
+  makeWidget(){
+    return `${this.first_name} ${this.last_name} Widget`
+  }
+  hire(newEmployee){
+    this.reports.push(newEmployee)
+  }
+  fire(index){
+    this.reports.splice(index, 1)
+  }
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -77,7 +104,53 @@
 
 //Code Here
 
-
+class ProgressiveManager {
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name
+    this.last_name = last_name
+    this.email = email
+    this.age = age
+    this.reports = []
+    this.title = 'Not a manager'
+    this.bonus = 0
+  }
+  makeWidget(){
+    return `${this.first_name} ${this.last_name} Widget`
+  }
+  hire(newEmployee){
+    this.reports.push(newEmployee)
+    if(this.reports.length<=0){
+      this.title = 'Not a manager'
+    }else if(this.reports.length<=3){
+      this.title = 'Barely Manager'
+    }else if(this.reports.length<=10){
+      this.title = 'Mostly Manager'
+    }else if(this.reports.length<=50){
+      this.title = 'Manager'
+    }else if(this.reports.length<=100){
+      this.title = 'Manager Plus'
+    }else{
+      this.title = 'Bestest Manager'
+    }
+  }
+  fire(index){
+    this.reports.splice(index, 1)
+    this.bonus+= 100
+    if(this.reports.length<=0){
+      this.title = 'Not a manager'
+    }else if(this.reports.length<=3){
+      this.title = 'Barely Manager'
+    }else if(this.reports.length<=10){
+      this.title = 'Mostly Manager'
+    }else if(this.reports.length<=50){
+      this.title = 'Manager'
+    }else if(this.reports.length<=100){
+      this.title = 'Manager Plus'
+    }else{
+      this.title = 'Bestest Manager'
+    }
+  }
+}
 
 ////////// PROBLEM 4 - Black Diamond //////////
 
