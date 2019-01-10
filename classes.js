12093299884890903764d6f103409f7f5b1191c0
elliot <elliotgcraig@icloud.com>
2019-01-09 21:21:33 -0700

    almost done

--- classes.js
+++ classes.js
@@ -177,4 +177,23 @@ class ProgressiveManager {
 
 //Code Here
 
-
+class Machine{
+  constructor(){
+    this.widgets_made_count = 0
+    this.wear_and_tear_count = 0
+    this.needs_reboot = false
+  }
+  makeWidgets(num){
+    this.widgets_made_count+=num
+    this.wear_and_tear_count+=this.widgets_made_count/50
+  }
+  fixMachine(){
+    this.needs_reboot = true
+  }
+  reboot(){
+    
+    return ()=>{
+      this.wear_and_tear_count-10
+      this.needs_reboot=false}
+  }
+}
